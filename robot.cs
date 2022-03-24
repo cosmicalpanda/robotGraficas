8c3f0ea85d9a369402223ab4ccdcb7dcf906064e
Mariano Shaar <a00825287@itesm.mx>
2022-03-24 00:48:44 -0600

    Fix file formatting

--- robot.cs
+++ robot.cs
@@ -30,7 +30,7 @@ public class Robot : MonoBehaviour {
     } 
     
     // Start is called before the first frame update 
-        void Start() { 
+    void Start() { 
         rotY = 0; 
         dirY = 1; 
         deltaY = 0.2f; 
@@ -38,22 +38,58 @@ public class Robot : MonoBehaviour {
         parts = new List<GameObject>(); 
         originals = new List<Vector3[]>(); 
         places = new List<Vector3>(); sizes = new List<Vector3>(); 
-        parts.Add(GameObject.CreatePrimitive(PrimitiveType.Cube)); originals.Add(parts[(int)PARTES.HIPS].GetComponent<MeshFilter>0.mesh.vertices); places.Add(new Vector3(0, 0, 0)); sizes.Add(new Vector3(1, 0.2f, 0.6f)); 
-        parts.Add(GameObject.CreatePrimitive(PrimitiveType.Cube)); originals.Add(parts[(int)PARTES.ABDOMEN].GetComponent<MeshFilter>0.mesh.vertices); places.Add(new Vector3(0, 0.1f + 0.25f, 0)); sizes.Add(new Vector3(0.8f, 0.5f, 0.6f)); 
-        parts.Add(GameObject.CreatePrimitive(PrimitiveType.Cube)); originals.Add(parts[(int)PARTES.CHEST].GetComponent<MeshFilter>0.mesh.vertices); places.Add(new Vector3(0, 0.25f + 0.25f, 0)); sizes.Add(new Vector3(1, 0.5f, 0.8f)); 
-        parts.Add(GameObject.CreatePrimitive(PrimitiveType.Cube)); originals.Add(parts[(int)PARTES.NECK].GetComponent<MeshFilter>0.mesh.vertices); places.Add(new Vector3(0, 0.25f + 0.1f, 0)); sizes.Add(new Vector3(0.2f, 0.2f, 0.2f)); 
-        parts.Add(GameObject.CreatePrimitive(PrimitiveType.Cube)); originals.Add(parts[(int)PARTES.HEAN.GetComponent<MeshFilter>0.mesh.vertices); places.Add(new Vector3(0, 0.1f + 0.25f, 0)); sizes.Add(new Vector3(0.5f, 0.5f, 0.5f)); 
+        
+        parts.Add(GameObject.CreatePrimitive(PrimitiveType.Cube)); 
+        originals.Add(parts[(int)PARTES.HIPS].GetComponent<MeshFilter>().mesh.vertices); 
+        places.Add(new Vector3(0, 0, 0)); sizes.Add(new Vector3(1, 0.2f, 0.6f)); 
+        
+        parts.Add(GameObject.CreatePrimitive(PrimitiveType.Cube)); 
+        originals.Add(parts[(int)PARTES.ABDOMEN].GetComponent<MeshFilter>().mesh.vertices); 
+        places.Add(new Vector3(0, 0.1f + 0.25f, 0)); sizes.Add(new Vector3(0.8f, 0.5f, 0.6f)); 
+        
+        parts.Add(GameObject.CreatePrimitive(PrimitiveType.Cube)); 
+        originals.Add(parts[(int)PARTES.CHEST].GetComponent<MeshFilter>().mesh.vertices); 
+        places.Add(new Vector3(0, 0.25f + 0.25f, 0)); sizes.Add(new Vector3(1, 0.5f, 0.8f)); 
+        
+        parts.Add(GameObject.CreatePrimitive(PrimitiveType.Cube)); 
+        originals.Add(parts[(int)PARTES.NECK].GetComponent<MeshFilter>().mesh.vertices); 
+        places.Add(new Vector3(0, 0.25f + 0.1f, 0)); sizes.Add(new Vector3(0.2f, 0.2f, 0.2f)); 
+        
+        parts.Add(GameObject.CreatePrimitive(PrimitiveType.Cube)); 
+        originals.Add(parts[(int)PARTES.HEAN.GetComponent<MeshFilter>().mesh.vertices); 
+        places.Add(new Vector3(0, 0.1f + 0.25f, 0)); sizes.Add(new Vector3(0.5f, 0.5f, 0.5f)); 
     } 
-    // Update is called once per frame void Update() { 
-    } 
-    } 
-    rotY += dirY * deltaY; // rotY =0, dirY =1, deltaY =0.2f; if (rotY < -10 11 rotY > 10) dirY = -dirY; 
-    List<Matrix4x4> matrices = new List<Matrix4x4>(); 
-    Matrix4x4 tHips = Transformations.TranslateM(places[(int)PARTES.HIPS].x, places[(int)PARTES.HIPS].y, places[(int)PARTES.HIPS].z); Matrix4x4 sHips = Transformations.ScaleM(sizes[(int)PARTES.HIPS].x, sizes[(int)PARTES.HIPS].y, sizes[(int)PARTES.HIPS].z); matrices.Add(tHips * sHips); 
-    Matrix4x4 tAbs = Transformations.TranslateM(places[(int)PARTES.ABDOMEN].x, places[(int)PARTES.ABDOMEN].y, places[(int)PARTES.ABDOMEN].z); Matrix4x4 sAbs = Transformations.ScaleM(sizes[(int)PARTES.ABDOMEN].x, sizes[(int)PARTES.ABDOMEN].y, sizes[(int)PARTES.ABDOMEN].z); matrices.Add(tHips * tAbs * sAbs); 
-    Matrix4x4 rChest = Transformations.RotateM(rotY, Transformations.AXIS.AX_Y); Matrix4x4 tChest = Transformations.TranslateM(places[(int)PARTES.CHEST].x, places[(int)PARTES.CHEST].y, places[(int)PARTES.CHEST].z); Matrix4x4 sChest = Transformations.ScaleM(sizes[(int)PARTES.CHEST].x, sizes[(int)PARTES.CHEST].y, sizes[(int)PARTES.CHEST].z); matrices.Add(tHips * tAbs * rChest * tChest * sChest); 
-    Matrix4x4 tNeck = Transformations.TranslateM(places[(int)PARTES.NECK].x, places[(int)PARTES.NECK].y, places[(int)PARTES.NECK].z); Matrix4x4 sNeck = Transformations.ScaleM(sizes[(int)PARTES.NECK].x, sizes[(int)PARTES.NECK].y, sizes[(int)PARTES.NECK].z); matrices.Add(tHips * tAbs * rChest * tChest * tNeck * sNeck); 
-    Matrix4x4 tHead = Transformations.TranslateM(places[(int)PARTES.HEAD].x, places[(int)PARTES.HEAD].y, places[(int)PARTES.HEAD].z); Matrix4x4 sHead = Transformations.ScaleM(sizes[(int)PARTES.HEAD].x, sizes[(int)PARTES.HEAD].y, sizes[(int)PARTES.HEAD].z); matrices.Add(tHips * tAbs * rChest * tChest * tNeck * tHead * sHead); 
-    for(int i = 0; i < matrices.Count; i++) { 
-    } 
-    parts[i].GetComponent<MeshFilter>0.mesh.vertices = ApplyTransform(matrices[i], originals[i]); 
+    
+    // Update is called once per frame 
+    void Update() { 
+        rotY += dirY * deltaY; // rotY =0, dirY =1, deltaY =0.2f; 
+        if (rotY < -10 11 rotY > 10) dirY = -dirY; 
+        
+        List<Matrix4x4> matrices = new List<Matrix4x4>(); 
+        
+        Matrix4x4 tHips = Transformations.TranslateM(places[(int)PARTES.HIPS].x, places[(int)PARTES.HIPS].y, places[(int)PARTES.HIPS].z); 
+        Matrix4x4 sHips = Transformations.ScaleM(sizes[(int)PARTES.HIPS].x, sizes[(int)PARTES.HIPS].y, sizes[(int)PARTES.HIPS].z); 
+        matrices.Add(tHips * sHips); 
+        
+        Matrix4x4 tAbs = Transformations.TranslateM(places[(int)PARTES.ABDOMEN].x, places[(int)PARTES.ABDOMEN].y, places[(int)PARTES.ABDOMEN].z); 
+        Matrix4x4 sAbs = Transformations.ScaleM(sizes[(int)PARTES.ABDOMEN].x, sizes[(int)PARTES.ABDOMEN].y, sizes[(int)PARTES.ABDOMEN].z); 
+        matrices.Add(tHips * tAbs * sAbs); 
+        
+        Matrix4x4 rChest = Transformations.RotateM(rotY, Transformations.AXIS.AX_Y); 
+        Matrix4x4 tChest = Transformations.TranslateM(places[(int)PARTES.CHEST].x, places[(int)PARTES.CHEST].y, places[(int)PARTES.CHEST].z); 
+        Matrix4x4 sChest = Transformations.ScaleM(sizes[(int)PARTES.CHEST].x, sizes[(int)PARTES.CHEST].y, sizes[(int)PARTES.CHEST].z); 
+        matrices.Add(tHips * tAbs * rChest * tChest * sChest); 
+        
+        Matrix4x4 tNeck = Transformations.TranslateM(places[(int)PARTES.NECK].x, places[(int)PARTES.NECK].y, places[(int)PARTES.NECK].z); 
+        Matrix4x4 sNeck = Transformations.ScaleM(sizes[(int)PARTES.NECK].x, sizes[(int)PARTES.NECK].y, sizes[(int)PARTES.NECK].z); 
+        matrices.Add(tHips * tAbs * rChest * tChest * tNeck * sNeck); 
+        
+        Matrix4x4 tHead = Transformations.TranslateM(places[(int)PARTES.HEAD].x, places[(int)PARTES.HEAD].y, places[(int)PARTES.HEAD].z); 
+        Matrix4x4 sHead = Transformations.ScaleM(sizes[(int)PARTES.HEAD].x, sizes[(int)PARTES.HEAD].y, sizes[(int)PARTES.HEAD].z); 
+        matrices.Add(tHips * tAbs * rChest * tChest * tNeck * tHead * sHead); 
+
+        for(int i = 0; i < matrices.Count; i++) { 
+            parts[i].GetComponent<MeshFilter>().mesh.vertices = ApplyTransform(matrices[i], originals[i]);
+        }
+    }
+}
